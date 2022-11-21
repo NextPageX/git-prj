8c08f75e7769f8525579b915ed7fcad5c358c13a
Liaoyj <venom0310@126.com>
2022-11-21 22:46:24 +0800

    First commit

---
+++ gitdemo/src/main/java/com/liaoyujie/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.liaoyujie.git;
+
+/**
+ * @Author liaoyujie
+ * @Date 2022-11-21 -下午 10:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
